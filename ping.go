b358c4034f17cdd6cc98d4e771aa3e4f7590eeda
Jeromy <why@ipfs.io>
2016-10-04 16:13:35 -0700

    extract host interface

--- ping.go
+++ ping.go
@@ -10,8 +10,8 @@ import (
 	u "github.com/ipfs/go-ipfs-util"
 	peer "github.com/ipfs/go-libp2p-peer"
 	logging "github.com/ipfs/go-log"
+	host "github.com/libp2p/go-libp2p-host"
 	inet "github.com/libp2p/go-libp2p-net"
-	host "github.com/libp2p/go-libp2p/p2p/host"
 )
 
 var log = logging.Logger("ping")
